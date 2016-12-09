4c377ed0f496e046eb30d1fbec58a40f04271332
u5788217 <5788217k@gmail.com>
2016-12-10 00:23:06 +0700

    Update login_db.php

--- login_db.php
+++ login_db.php
@@ -1,13 +1,15 @@
 <?php
-
 require_once 'config.php';
-
+session_start();
 try {
     $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
     $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	foreach ($oConn->query('SELECT * FROM account') as $aRow) {
-        if($_GET['email'] == $aRow['email'] && $_GET['pwd'] == $aRow['password'])
+        if($_GET['email'] == $aRow['email'] && $_GET['pwd'] == $aRow['password']){
+		$_SESSION["userid"] = $aRow['idaccount'];
 		header('Location: /index.php');
+	}
+		
     }
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
