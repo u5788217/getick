5f3fa9f6f9af37562470e104b3fd93eb5185d5ff
u5788217 <5788217k@gmail.com>
2016-12-10 00:07:17 +0700

    Update test.php

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ try {
     $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
     $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	foreach ($oConn->query('SELECT * FROM account') as $aRow) {
-        echo $aRow['userid'].$aRow['name']."<br>";
+        echo $aRow['idaccount'].$aRow['Fname']."<br>";
     }
 echo "hello";
 } catch(PDOException $e) {
