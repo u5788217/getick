cbc222ffc59f312d296c25d4534f6cdb6b4df524
u5788217 <5788217k@gmail.com>
2016-12-10 22:14:34 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,4 +3,5 @@ $sUsername = 'b10af5bf5dcca9';
 $sPassword = '65a62b24';
 $sHost = 'ap-cdbr-azure-southeast-b.cloudapp.net';
 $sDb = 'getick_database';
+$condb = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
 ?>
