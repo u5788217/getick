10d2042c7937ee58301736b2638c06d7b923034f
u5788217 <5788217k@gmail.com>
2016-12-10 22:35:44 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,17 @@
 <?php
-$sUsername = 'b10af5bf5dcca9';
-$sPassword = '65a62b24';
-$sHost = 'ap-cdbr-azure-southeast-b.cloudapp.net';
-$sDb = 'getick_database';
-$condb = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+function connect_db(){
+    $host = 'ap-cdbr-azure-southeast-b.cloudapp.net';
+    $database_name = 'getick_database';
+    $user = 'b10af5bf5dcca9';
+    $pass = '65a62b24';
+    try{
+        $conn = new PDO("mysql:host=$host;dbname=$database_name;charset=utf8", $user, $pass);
+        $conn->exec("set names utf8");
+        return $conn;
+    }
+    catch(PDOException $e){
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+}
 ?>
