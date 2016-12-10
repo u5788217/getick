8dd3e402f7bbecefacd6a34bbe2322850b88ddcc
u5788217 <5788217k@gmail.com>
2016-12-10 10:38:26 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,8 @@
+<?php
+	echo '<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>GETICK</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/mobile.css" media="screen and (max-width : 568px)">
+	<script type="text/javascript" src="js/mobile.js"></script>';
+?>
