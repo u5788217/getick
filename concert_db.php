b9002143ed27ea5d28d3c0ef6c8b361d7f1d9716
u5788217 <5788217k@gmail.com>
2016-12-10 23:36:19 +0700

    Update concert_db.php

--- concert_db.php
+++ concert_db.php
@@ -4,13 +4,13 @@ include 'config.php';
    $oConn = connect_db();
 	foreach ($oConn->query('SELECT * FROM concert') as $aRow) {
         	$conid = $aRow['id_concert'];
-		echo $conid.' <li>
+		echo '<li>
 		 	<a href="" class="figure">
 			<img src="'.$aRow['poster_concert'].'" alt="">
 			</a>
 			<div><h3>'.$aRow['name_concert'].'</h3>
 			<p>'.$aRow['des_concert'].'</p><form method = "get" action=""><select>';
-		foreach ($oConn->query('SELECT * FROM round') as $aRow) {
+		foreach ($oConn->query('SELECT * FROM round where id_concert == $conid') as $aRow) {
 			echo '<option value="'.$aRow['id_round'].'">'.$aRow['date'].' '.$aRow['time'].'</option>';
 		}
         echo '</select><input type="submit" value = "Book now"class="more">
