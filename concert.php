7932b8f35f9ccc85ce9ca401f02208c371485ee0
u5788217 <5788217k@gmail.com>
2016-12-10 23:46:51 +0700

    Update concert.php

--- concert.php
+++ concert.php
@@ -13,63 +13,8 @@ session_start();
 		
 		<h1><span>Concert Lists</span></h1>
 		<div>
-			<ul>
-				
-				
+			<ul>	
 			<?php include 'concert_db.php';?>
-				<li>
-					<a href="blog-single-post.html" class="figure">
-						<img src="images/concert/1coldplay.jpg" alt="">
-					</a>
-					<div>
-						<h3>Coldplay</h3>
-						<p>
-							Coldplay are a British rock band formed in 1996 by lead vocalist and keyboardist Chris Martin and lead guitarist Jonny Buckland at University College London (UCL).
-						</p>
-                        <h3>Date: 20 Jan 2017</h3>
-                        <h3>Time: 10:00 ~ 15:00</h3>
-						<a href="blog-single-post.html" class="more">Book now</a>
-					</div>
-				</li>
-				<li>
-					<a href="blog-single-post.html" class="figure">
-						<img src="images/concert/2Linkin-Park.png" alt="">
-					</a>
-					<div>
-						<h3>Linkin Park</h3>
-						<p>Linkin Park is an American rock band from Agoura Hills, California, the band rose to international fame with their debut album Hybrid Theory (2000), which was certified diamond by the RIAA in 2005.
-						</p>
-                        <h3>Date: 20 Jan 2017</h3>
-                        <h3>Time: 10:00 ~ 15:00</h3>
-						<a href="blog-single-post.html" class="more">Book now</a>
-					</div>
-				</li>
-                <li>
-					<a href="blog-single-post.html" class="figure">
-						<img src="images/concert/3Adele.jpg" alt="">
-					</a>
-					<div>
-						<h3>Adele Laurie</h3>
-						<p>Adele Laurie Blue Adkins MBE is an English singer-songwriter. Graduating from the BRIT School for Performing Arts and Technology in 2006, Adele was given a recording contract by XL Recordings.
-						</p>
-                        <h3>Date: 20 Jan 2017</h3>
-                        <h3>Time: 10:00 ~ 15:00</h3>
-						<a href="blog-single-post.html" class="more">Book now</a>
-					</div>
-				</li>
-                <li>
-					<a href="blog-single-post.html" class="figure">
-						<img src="images/concert/4alternative-rock.jpg" alt="">
-					</a>
-					<div>
-						<h3>Alternative rock</h3>
-						<p>Alternative rock is a genre of rock music that emerged from the independent music underground of the 1980s and became widely popular in the 1990s and 2000s.
-						</p>
-                        <h3>Date: 20 Jan 2017</h3>
-                        <h3>Time: 10:00 ~ 15:00</h3>
-						<a href="blog-single-post.html" class="more">Book now</a>
-					</div>
-				</li>
 			</ul>
 		</div>
 	</div>
