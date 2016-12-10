e19295cb3ce0aa3fe95c95de1cdd96fce291bcfd
u5788217 <5788217k@gmail.com>
2016-12-10 20:18:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,66 +9,68 @@ session_start();
 </head>
 <body>
 	<?php include 'header_menu.php';?>
-	<div id="body">
-		<h1><span>Concert Lists</span></h1>
-		<div>
-			<ul>
-				<li>
-					<a href="blog-single-post.html" class="figure">
-						<img src="images/concert/1coldplay.jpg" alt="">
-					</a>
-					<div>
-						<h3>Coldplay</h3>
-						<p>
-							Coldplay are a British rock band formed in 1996 by lead vocalist and keyboardist Chris Martin and lead guitarist Jonny Buckland at University College London (UCL).
-						</p>
-                        <h3>Date: 20 Jan 2017</h3>
-                        <h3>Time: 10:00 ~ 15:00</h3>
-						<a href="blog-single-post.html" class="more">Book now</a>
-					</div>
-				</li>
-				<li>
-					<a href="blog-single-post.html" class="figure">
-						<img src="images/concert/2Linkin-Park.png" alt="">
-					</a>
-					<div>
-						<h3>Linkin Park</h3>
-						<p>Linkin Park is an American rock band from Agoura Hills, California, the band rose to international fame with their debut album Hybrid Theory (2000), which was certified diamond by the RIAA in 2005.
-						</p>
-                        <h3>Date: 20 Jan 2017</h3>
-                        <h3>Time: 10:00 ~ 15:00</h3>
-						<a href="blog-single-post.html" class="more">Book now</a>
-					</div>
-				</li>
-                <li>
-					<a href="blog-single-post.html" class="figure">
-						<img src="images/concert/3Adele.jpg" alt="">
-					</a>
-					<div>
-						<h3>Adele Laurie</h3>
-						<p>Adele Laurie Blue Adkins MBE is an English singer-songwriter. Graduating from the BRIT School for Performing Arts and Technology in 2006, Adele was given a recording contract by XL Recordings.
-						</p>
-                        <h3>Date: 20 Jan 2017</h3>
-                        <h3>Time: 10:00 ~ 15:00</h3>
-						<a href="blog-single-post.html" class="more">Book now</a>
-					</div>
-				</li>
-                <li>
-					<a href="blog-single-post.html" class="figure">
-						<img src="images/concert/4alternative-rock.jpg" alt="">
-					</a>
-					<div>
-						<h3>Alternative rock</h3>
-						<p>Alternative rock is a genre of rock music that emerged from the independent music underground of the 1980s and became widely popular in the 1990s and 2000s.
-						</p>
-                        <h3>Date: 20 Jan 2017</h3>
-                        <h3>Time: 10:00 ~ 15:00</h3>
-						<a href="blog-single-post.html" class="more">Book now</a>
-					</div>
-				</li>
-			</ul>
-		</div>
-	</div>
+	<div class="banner">
+        <img src="logo1.png" style="height:100px; margin: 0 100px 0 100px;">
+        <ul>
+            <li style="margin-left: 100px;"><a href="#">Home</a></li>
+            <li><a href="#">Concert</a></li>
+            <li style="float:right; margin-right: 100px;"><a class="active" href="#about">About</a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <center><img src="coldplay_main2.png" style="width: 1320px;"></center>
+         <div class="content-right">
+            <div class="login">
+                <h3>Login</h3><br>
+                <label><b>Username eee</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required>
+
+                <label><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required>
+        
+                <button type="submit">Login</button>
+            </div>
+        </div>
+        <div class="content-left">
+            <h2>Recommended Events</h2>
+            <div class="conleft-pic">
+                <a href="#"><img src="concert/1coldplay.jpg"><br>Coldplay</a>
+            </div>
+            <div class="conleft-pic">
+                <a href="#"><img src="concert/2Linkin-Park.png"><br>Linkin-Park</a>
+            </div>
+            <div class="conleft-pic">
+                <a href="#"><img src="concert/3Adele.jpg"><br>Adele</a>
+            </div>
+            <div class="conleft-pic">
+                <a href="#"><img src="concert/4alternative-rock.jpg"><br>alternative-rock</a>
+            </div>
+            <div class="conleft-pic">
+                <a href="#"><img src="concert/5soundofmusic.jpg"><br>soundofmusic</a>
+            </div>
+        </div>
+        
+        <img src="ad.jpg" style="float: right;">
+        <div class="content-left-bottom">
+            <h2>Hot Events</h2>
+            <div class="conleft-pic">
+                <a href="#"><img src="concert/6Fourpandin.jpg"><br>Fourpandin</a>
+            </div>
+            <div class="conleft-pic">
+                <a href="#"><img src="concert/7Bird.jpg"><br>Bird</a>
+            </div>
+            <div class="conleft-pic">
+                <a href="#"><img src="concert/8bodyslam_nanglen_ver2_xlg.jpg"><br>bodyslam nanglen</a>
+            </div>
+            <div class="conleft-pic">
+                <a href="#"><img src="concert/9bie.jpg"><br>bie</a>
+            </div>
+            <div class="conleft-pic">
+                <a href="#"><img src="concert/10poster.jpg"><br>poster</a>
+            </div>
+        </div>
+       
+    </div>
 	<?php include 'footer.php';?>
 </body>
 </html>
