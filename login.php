fbd69e9bc886958fada62518fac9e4ec3e1be009
u5788217 <5788217k@gmail.com>
2016-12-10 10:36:51 +0700

    Update login.php

--- login.php
+++ login.php
@@ -10,22 +10,7 @@
 	<script type="text/javascript" src="js/mobile.js"></script>
 </head>
 <body>
-	<div id="header">
-		<a href="index.html" class="logo">
-			<img src="images/logo.jpg" alt="">
-		</a>
-		<ul id="navigation">
-			<li class="selected">
-				<a href="index.php">home</a>
-			</li>
-			<li>
-				<a href="concert.html">concerts</a>
-			</li>
-			<li>
-				<a href="login.php">login</a>
-			</li>
-		</ul>
-	</div>
+	<?php include 'header_menu.php';?>
 	<div id="body">
 		<h1><span>let's Login</span></h1>
 		<form method="get" action="login_db.php">
