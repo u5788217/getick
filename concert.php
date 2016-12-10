9f5296712bcfe11f1483d4da1c0b8dfcd3857d23
u5788217 <5788217k@gmail.com>
2016-12-10 10:28:54 +0700

    Update concert.php

--- concert.php
+++ concert.php
@@ -13,31 +13,7 @@ session_start();
 	<script type="text/javascript" src="js/mobile.js"></script>
 </head>
 <body>
-	<div id="header">
-		<a href="index.html" class="logo">
-			<img src="images/logo.jpg" alt="">
-		</a>
-		<ul id="navigation">
-			<li class="selected">
-				<a href="index.php">home</a>
-			</li>
-			<li>
-				<a href="concert.php">concerts</a>
-			</li>
-			
-			<?php if (!$_SESSION["userid"]) { ?>
-			<li>
-				<a href="login.php">login</a>
-			</li>
-			<?php } ?>
-			
-			<?php if ($_SESSION["userid"]) { ?>
-			<li>
-				<a href="logout.php">logout</a>
-			</li>
-			<?php } ?>
-		</ul>
-	</div>
+	<?php include 'header_menu.php';?>
 	<div id="body">
 		<h1><span>blog</span></h1>
 		<div>
