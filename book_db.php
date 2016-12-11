d302d49272b6a86f0d2ef71696a4b8c9e67f9863
u5788217 <5788217k@gmail.com>
2016-12-11 20:19:00 +0700

    Update book_db.php

--- book_db.php
+++ book_db.php
@@ -15,16 +15,21 @@ include 'config.php';
                                 <td>Available seat</td>
                             </tr>';
 		foreach ($oConn->query("SELECT * FROM zone WHERE id_round = '$roundid'") as $aRow3) {
-			$zoneid = $aRow3['id_zone'];
 			echo	'<tr>
                         <td>'.$aRow3['name_zone'].'</td>
                         <td>'.$aRow3['price'].'</td>
+			//ต้องมาหาที่ว่างทีหลัง
                         <td>30</td>
                      </tr>';
 		}
 	}
 	echo '</table></div></li><li><div style="width: 940px;" class="seat-style"><table>';
-  	foreach ($oConn->query("SELECT * FROM seat WHERE id_zone = '$roundid'") as $aRow) {
+  	foreach ($oConn->query("SELECT * FROM zone WHERE id_round = '$roundid'") as $aRow3) {
+		$zoneid = $aRow3['id_zone'];
+	foreach ($oConn->query("SELECT * FROM seat WHERE id_zone = '$zoneid'") as $aRow) {
+		
+		
+		
         switch ($aRow['id_zone']) {
 		case 1:case 2:case 6:case 10:case 14:case 15:
                 for($i=1; $i<=8; $i++){
@@ -32,51 +37,47 @@ include 'config.php';
                         case 1:
                             echo '<tr><td colspan="6"></td><td colspan="8" style="background-color: white;">STAGE</td>
                             <td colspan="6"></td></tr>';
-			    case 2:case 4:case 6:case 8:
+			case 2:case 4:case 6:case 8:
                             echo '<tr><td colspan="20"></td></tr>';
                         case 3:
 			     echo '<tr><td colspan="1"></td> '.$j.'A';
                              for($j=1; $j<=56; $j++){
-                                if(strcmp($aRow['$id_seat'],$j.'A') == 0){
                                     if($j==8 || $j==22 || $j==36 || $j==50){echo '<td colspan="4"></td>';}
                                     if($aRow['$id_booking'] == NULL){
                                         echo '<td id="A">'.$aRow['$id_seat'].'<br><input type="checkbox" value="'.$aRow['$id_seat'].'"></td>';
                                     } 
-                                    else{echo '<td id="A">'.$aRow['$id_seat'].'<br></td>';}
-                                    if($j%14==0){echo '<td colspan="1"></td><tr><td colspan="1"></td>';}
-                                }
+                                    else {echo '<td id="A">'.$aRow['$id_seat'].'<br></td>';}
+                                    if($j%14==0) {echo '<td colspan="1"></td><tr><td colspan="1"></td>';}
                             }
                             echo '</tr>';
                         case 5:
                             echo '<tr><td colspan="1"></td>';
                             for($j=1; $j<=96; $j++){
-                                if(strcmp($aRow['$id_seat'],$j.'B') == 0){
-                                    if($j==9 || $j==25 || $j==41 || $j==57 || $j==73 || $j ==89){echo '<td colspan="4"></td>';}
+                                    if($j==9 || $j==25 || $j==41 || $j==57 || $j==73 || $j ==89){echo '<td colspan="2"></td>';}
                                     if($aRow['$id_booking'] == NULL){
                                         echo '<td id="B">'.$aRow['$id_seat'].'<br><input type="checkbox" value="'.$aRow['$id_seat'].'"></td>';
                                     }   
                                     else{echo '<td id="B">'.$aRow['$id_seat'].'<br></td>';}
                                     if($j%16==0){echo '<td colspan="1"></td><tr><td colspan="1"></td>';}
-                                }
                             }
                             echo '</tr>';
                         case 7:
                             echo '<tr><td colspan="1"></td>';
                             for($j=1; $j<=90; $j++){
-				if(strcmp($aRow['$id_seat'],$j.'C') == 0){
                                     if($aRow['$id_booking'] == NULL){
                                         echo '<td id="C">'.$aRow['$id_seat'].'<br><input type="checkbox" value="'.$aRow['$id_seat'].'"></td>';
                                     }
                                     else{echo '<td id="C">'.$aRow['$id_seat'].'<br></td>';}
                                     if($j%18==0){echo '<td colspan="1"></td><tr><td colspan="1"></td>';}
-                           	    }
                             }
                             echo '</tr>';                           
                       }
-                }
-			break;
-        	}
+                   }
+		break;
+              }
             echo '</table></div></li>';
     	}
+	}
+
 	$oConn = null;
 ?>
