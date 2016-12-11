fdbaead732fa83196a77923d2a8558ac69698297
u5788217 <5788217k@gmail.com>
2016-12-11 23:39:25 +0700

    Update book.php

--- book.php
+++ book.php
@@ -29,7 +29,7 @@ function showCheck(str) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
+      document.getElementById("demo").innerHTML +=
       this.responseText;
     }
   };
