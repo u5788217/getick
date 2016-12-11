c8ae8223346b947606dfdc0306f4b49275852c15
u5788217 <5788217k@gmail.com>
2016-12-11 22:14:47 +0700

    Update book.php

--- book.php
+++ book.php
@@ -11,6 +11,13 @@
 		<div>
 			<ul>
 				<?php include 'book_db.php';?>
+				<li>
+                    <div style="margin-left: 200px;">
+                        <h2>Selected seat: A20 A21</h2>
+                        <h2>Total price: 10000 Baht</h2>
+                        <input type="button" value="BOOK" name="BOOK">
+                    </div>
+                </li>
 				<?php include 'book_ajax.php';?>
 			</ul>
 		</div>
