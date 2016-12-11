1740869528d6638f5a141714ba9e6c9e3c8cec7e
u5788217 <5788217k@gmail.com>
2016-12-11 12:58:30 +0700

    Add files via upload

---
+++ book.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<!-- Website Template by freewebsitetemplates.com -->
+<html>
+<head>
+	<?php include 'header.php';?>
+</head>
+<body>
+	<?php include 'header_menu.php';?>
+	<div id="body">
+		<h1><span>Booking ticket</span></h1>
+		<div>
+			<ul>
+				<?php include 'book_db.php';?>
+			</ul>
+		</div>
+	</div>
+	<?php include 'footer.php';?>
+</body>
+</html>
---
+++ book_db.php
@@ -0,0 +1,46 @@
+<?php
+include 'config.php';
+   $oConn = connect_db();
+   $roundid = $_GET['roundid'];
+	foreach ($oConn->query("SELECT * FROM round WHERE id_round = '$roundid'") as $aRow) {
+		$conid = $aRow['id_concert'];
+		foreach ($oConn->query("SELECT * FROM concert WHERE id_concert = '$conid'") as $aRow2) {
+			echo	'<div><h3>'.$aRow2['name_concert'].'</h3>
+					<p style="margin:0; padding:0;">'.$aRow2['des_concert'].'</p>';
+		}
+		echo '<h2>'.$aRow['date'].'<br> Time: '.$aRow['time'].'</h2><table border=1 style="width:300px; margin-left: 130px;">
+                            <tr>
+                                <td>ZONE</td>
+                                <td>Price(Baht)</td>
+                                <td>Available seat</td>
+                            </tr>';
+		foreach ($oConn->query("SELECT * FROM zone WHERE id_round = '$roundid'") as $aRow3) {
+			$zoneid = = $aRow3['id_zone'];
+			echo	'<tr>
+                        <td>'.$aRow3['name_zone'].'</td>
+                        <td>'.$aRow3['price'].'</td>
+                        <td>30</td>
+                     </tr>';
+		}
+	}
+	echo '</table></div>';
+	$oConn = null;
+?>
+
+
+      
+                        
+							
+							
+                             <tr>
+                                <td>B</td>
+                                <td>4000</td>
+                                <td>40</td>
+                            </tr>
+							
+                             <tr>
+                                <td>C</td>
+                                <td>2500</td>
+                                 <td>50</td>
+                            </tr>
+      
