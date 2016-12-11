d59e32279f095c324e02b072db38f4894771184d
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-12 00:49:52 +0700

    Create payment.php

---
+++ payment.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<!-- Website Template by freewebsitetemplates.com -->
+<html>
+<head>
+	<?php include 'header.php';?>
+</head>
+<body>
+	<?php include 'header_menu.php';?>
+	<div id="body">
+		
+		<h1><span>Concert Lists</span></h1>
+		<div>
+			<ul>	
+			 <?php include 'payment_db.php';?>
+			</ul>
+		</div>
+	</div>
+	<?php include 'footer.php';?>
+</body>
+</html>
