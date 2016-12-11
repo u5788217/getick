d77755b504fc29e1f22ebadf0753c87b9eb18d45
u5788217 <5788217k@gmail.com>
2016-12-11 23:16:12 +0700

    Add files via upload

---
+++ book_ajax.php
@@ -0,0 +1,14 @@
+<?php
+include 'config.php';
+   $oConn = connect_db();
+   $id = explode("&",$_GET['str']);
+   $seatid = $id[0];
+   $zoneid = $id[1];
+   $total =0;
+	foreach ($oConn->query("SELECT * FROM zone WHERE id_zone = '$zoneid'") as $aRow2) {
+		$total+=$aRow2['price'];
+	}
+	echo $seatid;
+	echo 'Total price : '.$total;
+	$oConn = null;
+?>
