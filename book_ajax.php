a4d3f627bbc53da953d03ac1ab8deb2967044bf5
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-12 00:07:16 +0700

    Update book_ajax.php

--- book_ajax.php
+++ book_ajax.php
@@ -2,8 +2,9 @@
 include 'config.php';
    $oConn = connect_db();
    $id = explode("+",$_GET['str']);
-   $zoneid = $id[1];
    $seatid = $id[0];
-  	echo $zoneid.'-'.$seatid.' ';
+   $zoneid = $id[1];
+  	echo $zoneid;
+	echo ' '.$seatid;
 	$oConn = null;
 ?>
