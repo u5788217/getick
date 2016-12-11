d671968975437734566738ce176f4b18279c9b3c
u5788217 <5788217k@gmail.com>
2016-12-11 22:49:55 +0700

    Update book_db.php

--- book_db.php
+++ book_db.php
@@ -47,7 +47,7 @@ include 'config.php';
 							$j=$aRow['id_seat'];
 							if($j==8 || $j==22 || $j==36 || $j==50){echo '<td colspan="4"></td>';}
 							if($aRow['id_booking'] == NULL){
-								echo '<td id="A">A'.$aRow['id_seat'].'<br><input type="checkbox" value="'.$aRow['id_seat'].'"></td>';
+								echo '<td id="A">A'.$aRow['id_seat'].'<br><input type="checkbox" name="A[]" value="'.$aRow['id_seat'].'"></td>';
 							} 
 						   	else {echo '<td id="A">A'.$aRow['id_seat'].'<br></td>';}
 							if($j%14==0) {
