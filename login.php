24d8923de397ff50487495a78b61450f398cb3f5
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-11 12:06:51 +0700

    Update login.php

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
 	<?php include 'header_menu.php';?>
 	<div id="body">
 		<h1><span>let's Login</span></h1>
-		<form method="get" action="login_db.php">
+		<form method="get" action="login_db.php" id="login">
 			<input type="text" name="email" id="email" placeholder="Email">
 			<input type="text" name="pwd" id="pwd" placeholder="password">
 			<input type="submit" name="send" id="send" value="send">
