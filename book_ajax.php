cd54d9c6764dac7322a5f36f771ce049cf1f8cc8
u5788217 <5788217k@gmail.com>
2016-12-12 00:25:00 +0700

    Update book_ajax.php

--- book_ajax.php
+++ book_ajax.php
@@ -1,14 +1,14 @@
 <?php
 include 'config.php';
    $oConn = connect_db();
-   $id = explode("+",$_GET['str']);
+   $id = explode(" ",$_GET['str']);
    $seatid = $id[0];
    $zoneid = $id[1];
    $price;
 	foreach ($oConn->query("SELECT * FROM zone WHERE id_zone = '$zoneid'") as $aRow2) {
 		$price=$aRow2['price'];
 	}
-	echo $_GET['str'];
+	echo $zoneid.$seatid;
 	echo 'Total price : '.$total;
 	$oConn = null;
 ?>
