250dc465f0c4ddf6ada3538efbf3919c7b8f6c1e
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-15 23:21:14 +0700

    Update adminconcert_db.php

--- adminconcert_db.php
+++ adminconcert_db.php
@@ -1,10 +1,9 @@
 <?php
 include 'config.php';
 $oConn = connect_db();
-   echo '<li>
-                    <div style="width:940px">
+   echo '<li><div style="width:940px">
                         <h3>All concert lists</h3>
-                        <h4>click concert's name to show available seat</h4>
+                        <h4>click concerts name to show available seat</h4>
                         <table style="width:850px; margin-left:50px;">
                             <tr>
                                 <td>
