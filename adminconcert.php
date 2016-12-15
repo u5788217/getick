df50616989307e392b70d61ada67b3dd06a65afb
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-15 23:10:20 +0700

    Create adminconcert.php

---
+++ adminconcert.php
@@ -0,0 +1,18 @@
+<?php
+include 'config.php';
+$oConn = connect_db();
+   echo '<li>
+                    <div style="width:940px">
+                        <h3>All concert lists</h3>
+                        <h4>click concert's name to show available seat</h4>
+                        <table style="width:850px; margin-left:50px;">
+                            <tr>
+                                <td>
+                                   
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </li>';
+	$oConn = null;
+?>
