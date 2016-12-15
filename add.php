1d9cebd96aa2ae90f45bf18411e742cdb119da89
u5788217 <5788217k@gmail.com>
2016-12-16 01:56:34 +0700

    Update add.php

--- add.php
+++ add.php
@@ -4,19 +4,10 @@ include 'config.php';
    $oConn = connect_db();
 $x = 109; 
 while($x <= 128) {
-    $oConn->query("INSERT INTO seat (id_seat,id_zone) VALUES ('$x', '7')");
+    $oConn->query("INSERT INTO seat (id_seat,id_zone) VALUES ('$x', '31')");
     $x++;
 }
-$x = 109; 
-while($x <= 128) {
-    $oConn->query("INSERT INTO seat (id_seat,id_zone) VALUES ('$x', '19')");
-    $x++;
-}
-$x = 109; 
-while($x <= 128) {
-    $oConn->query("INSERT INTO seat (id_seat,id_zone) VALUES ('$x', '21')");
-    $x++;
-}
+
 $oConn = null;
 ?>
 
