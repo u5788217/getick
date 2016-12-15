1536bafd7c9c719d1293910bf790eaa1ecee1d22
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-16 01:30:11 +0700

    Update register.php

--- register.php
+++ register.php
@@ -9,7 +9,11 @@
 	<div id="body">
 		<h1><span>Registration</span></h1>
 		<div>
-			
+			<form method="get" action="login_db.php">
+				<input type="text" name="fname" id="fname" placeholder="fname" class="login"><br>
+				<input type="text" name="email" id="email" placeholder="Email" class="login"><br>
+				<input type="text" name="pwd" id="pwd" placeholder="password" class="login"><br>
+			</form>
 		</div>
 		<input type="submit" name="send" id="send" value="Register">
 	</div>
