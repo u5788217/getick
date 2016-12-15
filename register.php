a9a44f81598fd5cd17583d8f52ecdd49adc07a54
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-16 01:21:28 +0700

    Create register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<!-- Website Template by freewebsitetemplates.com -->
+<html>
+<head>
+	<?php include 'header.php';?>
+</head>
+<body>
+	<?php include 'header_menu.php';?>
+	<div id="body">
+		<h1><span>Registration</span></h1>
+			<input type="text" name="email" id="email" placeholder="Email" class="login">
+			<input type="text" name="pwd" id="pwd" placeholder="password" class="login">
+		<input type="submit" name="send" id="send" value="Register">
+	</div>
+	<?php include 'footer.php';?>
+</body>
+</html>
