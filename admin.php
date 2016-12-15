e3d5bff95933a7224b70d73b1b76e1eedfffdf19
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-15 23:09:01 +0700

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<!-- Website Template by freewebsitetemplates.com -->
+<html>
+<head>
+	<?php include 'header.php';?>
+</head>
+<body>
+	<?php include 'header_menu.php';?>
+	<div id="body">
+		
+		<h1><span>Admin</span></h1>
+		<div>
+			<ul>	
+			 <?php include 'adminconcert_db.php';?>
+			</ul>
+		</div>
+	</div>
+	<?php include 'footer.php';?>
+</body>
+</html>
