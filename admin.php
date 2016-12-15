9542b31ce17bfd1b30a31aa56ec5b46b7868f92e
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-16 01:08:55 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -11,7 +11,7 @@ session_start();
 	<?php include 'header_menu.php';?>
 	<div id="body">
 		
-		<h1><span><a href="admin.php" style="text-decoration: underline;display: inline-block; color:#fff;">Admin</a></span></h1>
+		<h1><span><a href="admin.php" style="text-decoration: none;display: inline-block; color:#fff;">Admin</a></span></h1>
 		<div>
 			<ul>	
 			 <?php include 'adminconcert_db.php';?>
