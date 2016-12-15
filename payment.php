5fea0bcd1115a3068ac0b7d4fab31bd3b6a5d043
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-15 23:56:25 +0700

    Update payment.php

--- payment.php
+++ payment.php
@@ -12,7 +12,7 @@ session_start();
 	<div id="body">
 		
 		<h1><span>Payment</span></h1>
-		<div> <form method="get" action="updateseat.php" style="margin: 0;">
+		<div> <form method="get" action="updateseat.php" style="margin: auto; padding: auto;">
 			<ul>	
 			 <?php include 'payment_db.php';?>
 			</ul>
