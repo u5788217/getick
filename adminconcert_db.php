a42564c9f737b1a56c2a1fe208a2b06171be4ba7
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-16 00:37:08 +0700

    Update adminconcert_db.php

--- adminconcert_db.php
+++ adminconcert_db.php
@@ -3,7 +3,7 @@ include 'config.php';
 $oConn = connect_db();
    echo '<li><div style="width:940px">
                         <h3>All concert lists</h3>
-                        <h4>click concerts name to show available seat</h4><form id="sentform" method="get" action="adminshowcon.php">
+                        <h4>click concerts name to show available seat</h4><form id="sentform" method="get" action="adminshowcon.php" style="margin-left:20px;">
                         <table border="1" style="width:850px; margin-left:50px;"><tr><th>ID</th>
                         <th>Name</th><th>Date</th><th>Time</th></tr>';
                         foreach ($oConn->query("SELECT * FROM concert INNER JOIN round ON concert.id_concert = round.id_concert") as $aRow) {
