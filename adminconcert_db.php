db80fbc13bc0b45f39efd182bf5de833c9746264
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-16 00:36:18 +0700

    Update adminconcert_db.php

--- adminconcert_db.php
+++ adminconcert_db.php
@@ -3,11 +3,13 @@ include 'config.php';
 $oConn = connect_db();
    echo '<li><div style="width:940px">
                         <h3>All concert lists</h3>
-                        <h4>click concerts name to show available seat</h4><form method="get" action="adminshowcon.php">
-                        <table border="1" style="width:850px; margin-left:50px;"><tr><th>ID</th><th>Name</th><th>Date</th><th>Time</th></tr>';
+                        <h4>click concerts name to show available seat</h4><form id="sentform" method="get" action="adminshowcon.php">
+                        <table border="1" style="width:850px; margin-left:50px;"><tr><th>ID</th>
+                        <th>Name</th><th>Date</th><th>Time</th></tr>';
                         foreach ($oConn->query("SELECT * FROM concert INNER JOIN round ON concert.id_concert = round.id_concert") as $aRow) {
-                            echo '<tr><td>'.$aRow['id_round'].'</td><td><a href="#">'.$aRow['name_concert'].'</a></td><td> '
-                               .$aRow['date'].'</td><td>'.$aRow['time'].'</td></tr>';
+                            echo '<input type="hidden" name="'.$aRow['id_round'].'" value="'.$aRow['id_round'].'" />
+                                 <tr><td>'.$aRow['id_round'].'</td><td><a href="#" onclick="document.getElementById('sentform').submit();">'
+                               .$aRow['name_concert'].'</a></td><td> '.$aRow['date'].'</td><td>'.$aRow['time'].'</td></tr>';
                         }
                   echo'</table></from></div></li>';
 $oConn = null;
