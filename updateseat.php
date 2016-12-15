5067908e5bb5ab4ecb120b4f48e31f14077280fd
u5788217 <5788217k@gmail.com>
2016-12-16 00:38:37 +0700

    Update updateseat.php

--- updateseat.php
+++ updateseat.php
@@ -12,7 +12,7 @@ include 'config.php';
 	foreach ($oConn->query("SELECT MAX(id_booking) AS max FROM booking") as $aRow) {
 		$max = $aRow['max'];
 		for($x = 0; $x < sizeof($seats); $x++){
-			$oConn->query("UPDATE seat SET id_booking ='$max' WHERE id_seat = '$seats[$x]' AND id_zone = '$zones[$x]");
+			$oConn->query("UPDATE seat SET id_booking ='$max' WHERE id_seat = '$seats[$x]' AND id_zone = '$zones[$x]'");
 		}
 	
 	}
