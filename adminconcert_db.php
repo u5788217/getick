ef1ca97cbb5462110a0748161063ef1ae25d0553
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-15 23:32:52 +0700

    Update adminconcert_db.php

--- adminconcert_db.php
+++ adminconcert_db.php
@@ -4,14 +4,11 @@ $oConn = connect_db();
    echo '<li><div style="width:940px">
                         <h3>All concert lists</h3>
                         <h4>click concerts name to show available seat</h4>
-                        <table style="width:850px; margin-left:50px;">
-                            <tr>
-                                <td>
-                                   123
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                </li>';
+                        <table border="1" style="width:850px; margin-left:50px;"><tr><th>ID</th><th>name</th><th>Date</th><th>Time</th></tr>';
+                        foreach ($oConn->query('SELECT * FROM concert INNER JOIN round ON concert.id_concert = round.id_concert') as $aRow) {
+                            echo "<tr><td>".$aRrow["id_concert"]."</td><td>".$aRow["name_concert"]."</td><td> "
+                               .$row["date"]."</td><td>".$row["time"]."</td></tr>";
+                        }
+                  echo'</table></div></li>';
 $oConn = null;
 ?>
