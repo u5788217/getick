d72714577953429df8cb369628bc26257c6267fc
u5788217 <5788217k@gmail.com>
2016-12-16 00:48:34 +0700

    Update adminconcert_db.php

--- adminconcert_db.php
+++ adminconcert_db.php
@@ -4,12 +4,12 @@ $oConn = connect_db();
    echo '<li><div style="width:940px">
                         <h3>All concert lists</h3>
                         <h4>click concerts name to show available seat</h4>
-                        <form id="sentform" method="get" action="adminshowcon.php">
+         
                         <table border="1" style="width:850px; margin-left:50px;"><tr><th>ID</th>
                         <th>Name</th><th>Date</th><th>Time</th></tr>';
                         foreach ($oConn->query("SELECT * FROM concert INNER JOIN round ON concert.id_concert = round.id_concert") as $aRow) {
                             echo '<input type="hidden" name="'.$aRow['id_round'].'" value="'.$aRow['id_round'].'" />
-                                 <tr><td>'.$aRow['id_round'].'</td><td><a href="#" onclick="document.getElementById('sentform').submit();">'
+                                 <tr><td>'.$aRow['id_round'].'</td><td><a href="adminshowcon.php?round='.$aRow['id_round'].'">'
                                .$aRow['name_concert'].'</a></td><td> '.$aRow['date'].'</td><td>'.$aRow['time'].'</td></tr>';
                         }
                   echo'</table></form></div></li>';
