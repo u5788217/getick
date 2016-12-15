9f876ccda01954b64c99603b54232a76be0606b7
u5788217 <5788217k@gmail.com>
2016-12-16 01:43:02 +0700

    Update login_db.php

--- login_db.php
+++ login_db.php
@@ -2,6 +2,14 @@
 require_once 'config.php';
 session_start();
 $oConn = connect_db();
+if($_GET['fname'] != null){
+	$Fname = $_GET['fname'];
+	$Lname = $_GET['lname'];
+	$Email = $_GET['email'];
+	$Pwd = $_GET['pwd'];
+	$Address = $_GET['address'];
+	$oConn->query("INSERT INTO account (Fname, Lname, email, address, password) VALUES ('$Fname', '$Lname','$Email','$Address','$Pwd')");
+}
 	foreach ($oConn->query('SELECT * FROM account') as $aRow) {
 		if($_GET['email'] == $aRow['email'] && $_GET['pwd'] == $aRow['password']){
 			$_SESSION["userid"] = $aRow['idaccount'];
