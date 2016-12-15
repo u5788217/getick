c3da477812d9571e2b80b43af7eac134169a9bb3
u5788217 <5788217k@gmail.com>
2016-12-15 22:51:40 +0700

    Update payment_db.php

--- payment_db.php
+++ payment_db.php
@@ -19,7 +19,6 @@ $time;
 $conname;
 $poster;
 $seatprint = array();
-echo "TESTTT";
 for($x = 0; $x < sizeof($zoneID); $x++){
 	foreach ($oConn->query('SELECT * FROM round WHERE id_zone = "$zoneID[$x]"') as $aRow) {
 		$total += $aRow['price'];
