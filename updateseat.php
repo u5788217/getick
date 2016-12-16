473d1686860f2f142933b8729be621033a3da586
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-16 12:28:39 +0700

    Update updateseat.php

--- updateseat.php
+++ updateseat.php
@@ -9,10 +9,10 @@ include 'config.php';
     $payment = $_GET['payment'];
 	if (!$_SESSION["userid"]) echo "<script type='text/javascript'>alert('You need to login.'); window.location = 'index.php';</script>";
 	$userID = $_SESSION["userid"];
-	$oConn->query("INSERT INTO booking (total_price, payment_method, id_account) VALUES ('$total', '$payment','$userID')");
 	foreach ($oConn->query("SELECT * FROM booking") as $aRow) {
 		if($aRow['id_booking']>$id) $id = $aRow['id_booking'];
 	}
+	$oConn->query("INSERT INTO booking (id_booking,total_price, payment_method, id_account) VALUES ('$id'.'$total', '$payment','$userID')");
 	for($x = 0; $x < sizeof($seats); $x++){
 		$oConn->query("UPDATE seat SET id_booking ='$id' WHERE id_seat = '$seats[$x]' AND id_zone = '$zones[$x]'");
 	}
