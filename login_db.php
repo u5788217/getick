1f68e0b02612d6b06f5851392e089be8f54e2d05
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-16 12:53:55 +0700

    Update login_db.php

--- login_db.php
+++ login_db.php
@@ -19,8 +19,8 @@ if($_GET['fname'] != null){
 		if($_GET['email'] == $aRow['email'] && $_GET['pwd'] == $aRow['password']){
 			$_SESSION["userid"] = $aRow['idaccount'];
 			if($_GET['fname'] != null) echo '<script language="javascript">
-			alert("Registration was done, now you are loging in."); </script>';
-			header('Location: /index.php');
+			alert("Registration was done, now you are loging in."); 
+			window.location = "index.php";</script>';
 		}
 	}  
 $oConn->execute();
