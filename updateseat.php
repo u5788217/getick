690db13295bc56eee64ac0cce1fa3a1277f22080
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-16 12:20:59 +0700

    Update updateseat.php

--- updateseat.php
+++ updateseat.php
@@ -5,17 +5,19 @@ include 'config.php';
    $total = $_GET['total'];
    $seats = $_GET['seat'];
    $zones = $_GET['zone'];
+   $id;
     $payment = $_GET['payment'];
 	if (!$_SESSION["userid"]) echo "<script type='text/javascript'>alert('You need to login.'); window.location = 'index.php';</script>";
 	$userID = $_SESSION["userid"];
 	$oConn->query("INSERT INTO booking (total_price, payment_method, id_account) VALUES ('$total', '$payment','$userID')");
-	foreach ($oConn->query("SELECT MAX(id_booking) AS max FROM booking") as $aRow) {
-		$max = $aRow['max'];
-		for($x = 0; $x < sizeof($seats); $x++){
-			$oConn->query("UPDATE seat SET id_booking ='$max' WHERE id_seat = '$seats[$x]' AND id_zone = '$zones[$x]'");
-		}
-	echo "<script type='text/javascript'>alert('Done.'); window.location = 'index.php';</script>";
+	foreach ($oConn->query("SELECT * FROM booking") as $aRow) {
+		if($aRow['id_booking']>$id)$id = $aRow['id_booking'];
 	}
+	for($x = 0; $x < sizeof($seats); $x++){
+		$oConn->query("UPDATE seat SET id_booking ='$id' WHERE id_seat = '$seats[$x]' AND id_zone = '$zones[$x]'");
+	}
+	echo '<script language="javascript">alert('Done.'); window.location = 'index.php';</script>';
+	
 	$oConn = null;
 ?>
 
