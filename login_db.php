852e3c1ed6477a479b9353dc87536b225241993c
suusuusuu00 <suusuusuu00@gmail.com>
2016-12-16 12:51:39 +0700

    Update login_db.php

--- login_db.php
+++ login_db.php
@@ -3,17 +3,23 @@ require_once 'config.php';
 session_start();
 $oConn = connect_db();
 if($_GET['fname'] != null){
+	$id;
 	$Fname = $_GET['fname'];
 	$Lname = $_GET['lname'];
 	$Email = $_GET['email'];
 	$Pwd = $_GET['pwd'];
 	$Address = $_GET['address'];
-	$oConn->query("INSERT INTO account (Fname, Lname, email, address, password) VALUES ('$Fname', '$Lname','$Email','$Address','$Pwd')");
+	foreach ($oConn->query("SELECT MAX(idaccount) FROM account") as $aRow) {
+		$id = $aRow[0]+1;
+	}
+	$oConn->query("INSERT INTO account (idaccount,Fname, Lname, email, address, password) 
+		VALUES ('$id','$Fname', '$Lname','$Email','$Address','$Pwd')");
 }
 	foreach ($oConn->query('SELECT * FROM account') as $aRow) {
 		if($_GET['email'] == $aRow['email'] && $_GET['pwd'] == $aRow['password']){
 			$_SESSION["userid"] = $aRow['idaccount'];
-			if($_GET['fname'] != null) echo "<script type='text/javascript'>alert('Registration was done, now you are loging in.'); </script>";
+			if($_GET['fname'] != null) echo '<script language="javascript">
+			alert("Registration was done, now you are loging in."); </script>';
 			header('Location: /index.php');
 		}
 	}  
